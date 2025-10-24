67398ab217d53ed17c51389c2d4255583f10fad2
Nirmala Bonami <90679647+bonami1@users.noreply.github.com>
2025-10-24 11:36:24 +0200

    Update boucles.c

---
+++ TP1/src/boucles.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    int compteur = 5;
+
+    for (int i = 1; i <= compteur; i++) {
+        for (int j = 1; j <= i; j++) {
+         
+            if (i % 2 == 0 && j > 1 && j < i) {
+                printf("# ");
+            } else {
+                printf("* ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
