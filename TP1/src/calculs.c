3b3b97b9e3f31c63763922091c02be8e607a0921
Nirmala Bonami <90679647+bonami1@users.noreply.github.com>
2025-10-24 11:54:49 +0200

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,66 @@
+#include <stdio.h>
 
+int main(void) {
+    int num1 = 12;
+    int num2 = 4;
+    char op = '+';  /* a changer pour essayer avec : '-', '*', '/', '%', '&', '|', '~' */
+
+    int resultat = 0;
+
+    printf("num1 = %d, num2 = %d, opérateur = '%c'\n\n", num1, num2, op);
+
+    switch (op) {
+        case '+':
+            resultat = num1 + num2;
+            printf("Résultat : %d + %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '-':
+            resultat = num1 - num2;
+            printf("Résultat : %d - %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '*':
+            resultat = num1 * num2;
+            printf("Résultat : %d * %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '/':
+            if (num2 != 0) {
+                resultat = num1 / num2;
+                printf("Résultat : %d / %d = %d\n", num1, num2, resultat);
+            } else {
+                printf("Erreur : division par zéro !\n");
+            }
+            break;
+
+        case '%':
+            if (num2 != 0) {
+                resultat = num1 % num2;
+                printf("Résultat : %d %% %d = %d\n", num1, num2, resultat);
+            } else {
+                printf("Erreur : modulo par zéro !\n");
+            }
+            break;
+
+        case '&':
+            resultat = num1 & num2;
+            printf("Résultat : %d & %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '|':
+            resultat = num1 | num2;
+            printf("Résultat : %d | %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '~':
+            printf("Résultat : ~%d = %d\n", num1, ~num1);
+            break;
+
+        default:
+            printf("Erreur : opérateur '%c' non reconnu !\n", op);
+            break;
+    }
+
+    return 0;
+}
