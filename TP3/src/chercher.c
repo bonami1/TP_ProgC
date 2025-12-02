e86ff86922524d48b00e7c79c5d4acbe5e74eea4
Nirmala Bonami <90679647+bonami1@users.noreply.github.com>
2025-12-02 11:46:50 +0100

    Update chercher.c

--- TP3/src/chercher.c
+++ TP3/src/chercher.c
@@ -6,32 +6,39 @@
 
 int main(void) {
     int tableau[SIZE];
-    int i, entierCherche;
-    int trouve = 0; 
+    int i;
+    int entierCherche;
+    int present = 0; // 0 = absent, 1 = présent
 
+    // Initialisation du générateur de nombres aléatoires
     srand(time(NULL));
 
+    // Remplissage du tableau avec des valeurs aléatoires entre -50 et 49
     for (i = 0; i < SIZE; i++) {
         tableau[i] = rand() % 100 - 50;
     }
 
+    // Affichage du tableau
     printf("Tableau :\n");
     for (i = 0; i < SIZE; i++) {
         printf("%d ", tableau[i]);
     }
     printf("\n\n");
 
+    // Demande de l'entier à chercher
     printf("Entrez l'entier que vous souhaitez chercher : ");
     scanf("%d", &entierCherche);
 
+    // Recherche de l'entier dans le tableau
     for (i = 0; i < SIZE; i++) {
         if (tableau[i] == entierCherche) {
-            trouve = 1;
-            break; 
+            present = 1;
+            break; // On arrête la boucle dès qu'on trouve l'entier
         }
     }
 
-    if (trouve) {
+    // Affichage du résultat
+    if (present) {
         printf("Résultat : entier présent\n");
     } else {
         printf("Résultat : entier absent\n");
@@ -39,3 +46,4 @@ int main(void) {
 
     return 0;
 }
+
